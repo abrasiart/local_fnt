5042dbe70367444a5b1e86cac9870edacc7368e8
abrasiart <abrasil.art@gmail.com>
2025-09-19 10:30:54 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    strictPort: true,
-    open: true
+    strictPort: true
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
   }
 });
