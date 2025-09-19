30cf82688a5ae79ab7e91f3caf741fe7920a0472
abrasiart <abrasil.art@gmail.com>
2025-09-19 10:37:50 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,1 @@
-// src/config.ts (Vite)
-export const API_BASE =
-  (import.meta as any).env.VITE_API_URL || "http://localhost:4000";
+export const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:4000";
